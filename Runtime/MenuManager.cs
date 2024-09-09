57c86ade35bed60fc622ad5336499f905c8bb3c5
RehcubGit <87893932+RehcubGit@users.noreply.github.com>
2024-09-09 23:00:00 +0200

    Batman

---
+++ Runtime/MenuManager.cs
@@ -0,0 +1,165 @@
+﻿using Rehcub.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.SceneManagement;
+
+namespace Rehcub.UI
+{
+    public class MenuManager : Singleton<MenuManager>
+    {
+        public MenuScreen CurrentScreen { get => _currentScreen; }
+        private MenuScreen _currentScreen;
+        [SerializeField] private MenuScreen _startScreen;
+
+        [Tooltip("If the MenuManager should use the cancle Action from the EventSystem to go back.")]
+        [SerializeField] private bool _backWithCancleAction = true;
+        [SerializeField] private InputSystemUIInputModule _module;
+
+        private Stack<MenuScreen> _previewsScreens = new Stack<MenuScreen>();
+        private List<MenuScreen> _availableScreens = new List<MenuScreen>();
+
+        public event Func<bool> InterceptBack;
+
+        private void Start()
+        {
+            _availableScreens.AddRange(FindObjectsOfType<MenuScreen>(true));
+
+            if(_availableScreens.Count == 0)
+            {
+                Debug.LogError("No Screens in the Scene, destroying MenuManager!");
+                Destroy(gameObject);
+                return;
+            }
+
+            foreach (MenuScreen menuScreen in _availableScreens)
+            {
+                menuScreen.Initialize();
+            }
+
+            /*if (TryGetComponent(out MenuAudioHandler audioHandler))
+                audioHandler.SetupAudio(_availableScreens);*/
+
+            if (_startScreen == null)
+            {
+                Debug.LogError("No Start Screen is defined!");
+                _startScreen = _availableScreens[0];
+            }
+
+            if(_startScreen.gameObject.activeSelf == false)
+                Debug.LogWarning("The Start Screen is not activ!");
+
+            SetCurrentScreen(_startScreen);
+        }
+
+        private void OnEnable()
+        {
+
+            if (_backWithCancleAction == false)
+                return;
+
+            if (_module == null)
+            {
+                BaseInputModule baseModule = EventSystem.current.currentInputModule;
+                if (baseModule is not InputSystemUIInputModule inputModule)
+                    return;
+
+                _module = inputModule;
+            }
+
+            _module.cancel.action.performed += Trigger;
+        }
+
+        private void OnDisable()
+        {
+            if (_backWithCancleAction == false)
+                return;
+
+            if (_module == null)
+                return;
+
+            _module.cancel.action.performed -= Trigger;
+        }
+
+        private void OnDestroy()
+        {
+            foreach (MenuScreen menuScreen in _availableScreens)
+                menuScreen.Destroy();
+        }
+
+        private void Trigger(InputAction.CallbackContext context)
+        {
+            bool? intercepted = InterceptBack?.Invoke();
+
+            if (intercepted.HasValue && intercepted.Value)
+                return;
+
+            if(context.started)
+                Back();
+        }
+
+        public void SetCurrentScreen(MenuScreen menuScreen)
+        {
+            if (_currentScreen != null)
+            {
+                if (_currentScreen.IsEndScreen)
+                    return;
+                _previewsScreens.Push(_currentScreen);
+                _currentScreen.Deselect();
+            }
+            _currentScreen = menuScreen;
+            _currentScreen.Select();
+        }
+
+        public void SwitchTo(MenuScreen menuScreen)
+        {
+            if (_currentScreen != null)
+                _currentScreen.Deselect();
+
+            _currentScreen = menuScreen;
+            _currentScreen.Select();
+        }
+
+        public void Back()
+        {
+            if (_previewsScreens.Count == 0)
+                return;
+
+            MenuScreen lastScreen = _currentScreen;
+            _currentScreen = _previewsScreens.Pop();
+
+            if (lastScreen != null)
+                lastScreen.Deselect();
+
+            _currentScreen.Select();
+        }
+
+        public void BackAll()
+        {
+            while (_previewsScreens.Count > 0)
+                Back();
+        }
+
+        public T GetScreenByType<T>() where T : MenuScreen
+        {
+            foreach (MenuScreen screen in _availableScreens)
+            {
+                if (screen is T t)
+                    return t;
+            }
+            Debug.LogError($"No Screen of type {typeof(T)} found!");
+            return null;
+        }
+
+        public bool IsPreviewsScreen(MenuScreen menuScreen) => _previewsScreens.Contains(menuScreen);
+
+        public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+        public void LoadScene(int index) => SceneManager.LoadScene(index);
+        public void ReloadCurrentScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        public void QuitGame() => Application.Quit();
+    }
+}
---
+++ Runtime/MenuScreen.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.EventSystems;
+#endif
+
+namespace Rehcub.UI
+{
+    public class MenuScreen : MonoBehaviour
+    {
+
+        #region Editor Functions
+#if UNITY_EDITOR
+
+        [MenuItem("GameObject/UI/Menu/Empty")]
+        public static void AddMenuScreen()
+        {
+            GameObject menu = AddMenu();
+
+            menu.AddComponent<MenuScreen>();
+        }
+
+        private static GameObject CreateMenuScreen()
+        {
+            GameObject obj = new GameObject
+            {
+                name = "Menu"
+            };
+
+            if (FindObjectOfType(typeof(MenuManager)) == null)
+            {
+                GameObject managerObj = new GameObject
+                {
+                    name = "MenuManager"
+                };
+                managerObj.AddComponent<MenuManager>();
+            }
+
+            return obj;
+        }
+
+        public static GameObject AddMenu()
+        {
+            GameObject menu;
+            GameObject parent = Selection.activeGameObject;
+            bool addWithCanvas = parent == null || (parent != null && parent.GetComponentInParent<Canvas>() == null);
+            if (addWithCanvas)
+            {
+                menu = AddOutsideCanvas();
+            }
+            else
+            {
+                if (parent.GetComponentInParent<MenuScreen>() != null)
+                {
+                    Debug.LogError("A MenuScreen can not be inside another MenuScreen!");
+                    return null;
+                }
+
+                menu = AddInsideCanvas();
+            }
+
+            if (parent != null)
+                menu.transform.SetParent(Selection.activeGameObject.transform, false);
+            Selection.activeGameObject = menu;
+
+            return menu;
+        }
+
+        private static GameObject AddInsideCanvas()
+        {
+            GameObject menu = CreateMenuScreen();
+            RectTransform rectTransform = menu.AddComponent<RectTransform>();
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+            return menu;
+        }
+
+        private static GameObject AddOutsideCanvas()
+        {
+            GameObject menu = CreateMenuScreen();
+            menu.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            menu.AddComponent<CanvasScaler>();
+            menu.AddComponent<GraphicRaycaster>();
+
+            if (FindObjectOfType<EventSystem>() == null)
+            {
+                GameObject eventSystem = new GameObject
+                {
+                    name = "EventSystem"
+                };
+                eventSystem.AddComponent<EventSystem>();
+                eventSystem.AddComponent<StandaloneInputModule>();
+            }
+            return menu;
+        }
+#endif
+        #endregion
+
+        public enum TransionMode
+        {
+            None, Activate, Fade, UIToolkit
+        }
+
+        [SerializeField] private TransionMode _transionMode;
+        [SerializeField] private Selectable _firstSelected;
+        public bool IsEndScreen { get => _isEndScreen; }
+        [Tooltip("When set you can not go further in the menu tree, you can only go back or switch to another")]
+        [SerializeField] private bool _isEndScreen;
+
+        public UnityEvent OnInitialize;
+        public UnityEvent OnSelect;
+        public UnityEvent OnDeselect;
+
+        private bool _isInitialized;
+
+        public bool IsSelected { get => _isSelected; }
+        private bool _isSelected;
+
+        [HideInInspector][SerializeField] private VisualElement _root;
+        
+        protected virtual void OnValidate()
+        {
+            if(TryGetComponent(out UIDocument document))
+            {
+                _root = document.rootVisualElement;
+                _transionMode = TransionMode.UIToolkit;
+            }
+            else if(_transionMode == TransionMode.UIToolkit)
+            {
+                _root = null;
+                _transionMode = TransionMode.None;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_isSelected)
+                return;
+
+            switch (_transionMode)
+            {
+                case TransionMode.None:
+                    break;
+                case TransionMode.Activate:
+                    gameObject.SetActive(false);
+                    break;
+                case TransionMode.Fade:
+                    break;
+                case TransionMode.UIToolkit:
+                    _root.style.display = DisplayStyle.None;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public virtual void Initialize()
+        {
+            _isInitialized = true;
+            OnInitialize?.Invoke();
+            if (_firstSelected == null)
+                Debug.LogWarning($"The menuscreen {gameObject.name} has not defined a first selected.");
+        }
+
+        public virtual void Select()
+        {
+            if (_isInitialized == false)
+                Initialize();
+
+            _isSelected = true;
+            switch (_transionMode)
+            {
+                case TransionMode.None:
+                    break;
+                case TransionMode.Activate:
+                    gameObject.SetActive(true);
+                    break;
+                case TransionMode.Fade:
+                    break;
+                case TransionMode.UIToolkit:
+                    _root.style.display = DisplayStyle.Flex;
+                    break;
+                default:
+                    break;
+            }
+            OnSelect?.Invoke();
+
+            if(_firstSelected != null)
+            {
+                _firstSelected.Select();
+            }
+        }
+
+        public virtual void Deselect()
+        {
+            _isSelected = false;
+            switch (_transionMode)
+            {
+                case TransionMode.None:
+                    break;
+                case TransionMode.Activate:
+                    gameObject.SetActive(false);
+                    break;
+                case TransionMode.Fade:
+                    break;
+                case TransionMode.UIToolkit:
+                    _root.style.display = DisplayStyle.None;
+                    break;
+                default:
+                    break;
+            }
+            OnDeselect?.Invoke();
+        }
+
+        public virtual void Destroy() { }
+    }
+}
